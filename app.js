87a384c36028c26348bea96821dc42ad0272ee5b
harwarl <harwarl87@gmail.com>
2024-08-15 11:04:27 +0100

    cors adjusted

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://escrow-app-ktym.onrender.com/"
+    "https://escrow-app-ktym.onrender.com"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
