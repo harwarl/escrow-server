fed2cce773e1d7a3b35d2a5e21fbc087aebe5eb6
harwarl <harwarl87@gmail.com>
2024-08-15 08:52:42 +0100

    access origin

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://escrow-server-4xle.onrender.com"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
